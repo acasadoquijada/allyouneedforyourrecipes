eeec7b511fa5416afdb1a0cd3350a324c1fbf5f6
acasadoquijada <acasadoquijada@gmail.com>
2021-03-07 10:45:52 +0100

    chore: passing recipes using event emitters
    The recipes are passed between the different components by using
    event emmiters and property binding.
    
    This will change in the future by the use of services.

--- allYouNeedForYourRecipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ allYouNeedForYourRecipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -17,4 +17,6 @@ export class RecipeListComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {}
 }
--- allYouNeedForYourRecipes/src/app/shopping-list/shopping-list.component.ts
+++ allYouNeedForYourRecipes/src/app/shopping-list/shopping-list.component.ts
@@ -8,10 +8,15 @@ import { Ingredient } from '../shared/ingredient.model';
 })
 export class ShoppingListComponent implements OnInit {
   ingredients: Ingredient[] = [
-    new Ingredient('apple', 4),
-    new Ingredient('Tomatoes', 5),
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
   ];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit() {}
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
